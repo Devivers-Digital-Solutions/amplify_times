296b6fbee8d9f0d916fed4c2e92465b3cbb21af2
Sakshi <sakshibagul0502@gmail.com>
2025-06-20 12:50:11 +0530

    Updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
+
+
 const Footer = () => {
   return (
     <footer className="w-full bg-transparent pt-16 pb-10 md:pb-20">
       <div className="bg-[#EE6F20] lg:rounded-[26px] rounded-[14px] lg:max-w-6xl lg:mx-auto mx-5 lg:pt-0 pt-[50px]">
-        <div className="border-l lg:border-l-2 lg:border-l-white lg:border-t-0 border-t-2 border-t-white lg:ml-[50px] lg:pl-[52px] lg:pr-[100px] lg:pt-[132px] lg:pb-[66px] pb-8 pt-11 px-3.5">
+        {/* <div className="border-l lg:border-l-2 lg:border-l-white lg:border-t-0 border-t-2 border-t-white lg:ml-[50px] lg:pl-[52px] lg:pr-[100px] lg:pt-[132px] lg:pb-[66px] pb-8 pt-11 px-3.5"> */}
+        <div
+      className="border-l lg:border-l-2 lg:border-l-white lg:border-t-0 border-t-2 border-t-white lg:ml-[50px] lg:pl-[52px] lg:pr-[100px] lg:pt-[132px] lg:pb-[66px] pb-8 pt-11 px-3.5"
+    >
           <p className="text-white md:text-[80px] text-[40px]">LET'S WORK</p>
           <p className="text-white md:text-[80px] text-[40px] flex justify-center items-center">
             TOGETHER
